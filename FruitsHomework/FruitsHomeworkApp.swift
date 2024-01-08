b4ef5a83dd3fc1b67107ac89c025eabeac9a1efc
Matvii Artemenko <100190098+MatveyArtemenko@users.noreply.github.com>
2024-01-08 21:04:16 +0000

    Initial Commit

---
+++ FruitsHomework/FruitsHomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsHomeworkApp.swift
+//  FruitsHomework
+//
+//  Created by Matvii Artemenko on 08/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct FruitsHomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
