fcafac1268c56d45e49bf663040a03e1ce2fef6f
Matvii Artemenko <100190098+MatveyArtemenko@users.noreply.github.com>
2024-01-11 23:27:45 +0000

    Wrote tests for different functionalities
    Wrote tests for functions in NetworkManager, StatisticsManager, FruitViewModel

--- FruitsHomework/StatisticsManager.swift
+++ FruitsHomework/StatisticsManager.swift
@@ -10,7 +10,8 @@ import Foundation
 class StatisticsManager: ObservableObject {
     @Published var startTime: DispatchTime?
 
-    func networkCallStats(block: () -> ()) {
+    @discardableResult
+    func networkCallStats(block: () -> ()) -> Double {
         let clock = ContinuousClock()
         let result = clock.measure {
             block()
@@ -18,12 +19,14 @@ class StatisticsManager: ObservableObject {
         let duration = result.description.trimmingCharacters(in: .letters)
         print("result = \(duration)")
         getStats(for: .load, "\(duration.trimmingCharacters(in: .whitespacesAndNewlines))")
+        return Double(result.components.seconds)
     }
 
-    func displayEventStats() {
+    @discardableResult
+    func displayEventStats() -> Double {
 
         guard let startTime = startTime else {
-            return
+            return 0.0
         }
         print("\(startTime.uptimeNanoseconds)")
         let endTime = DispatchTime.now()
@@ -33,6 +36,7 @@ class StatisticsManager: ObservableObject {
         print("Display event: \(elapsedTimeInMilliSeconds) ms")
         getStats(for: .display, String(elapsedTimeInMilliSeconds))
 
+        return elapsedTimeInMilliSeconds
     }
     
     func errorEventStats(block: () throws -> ()) {
@@ -45,14 +49,16 @@ class StatisticsManager: ObservableObject {
         
     }
 
-    func getStats(for event: ScreeningEvents, _ data: String) {
+    @discardableResult
+    func getStats(for event: ScreeningEvents, _ data: String) -> String {
         let urlString = "https://raw.githubusercontent.com/fmtvp/recruit-test-data/master/stats?event=\(event.rawValue)&data=\(data)"
 //        print("Url: \(urlString)")
         guard let url = URL(string: urlString) else {
-            return
+            return ""
         }
 
         Network.shared.sendData(with: url)
+        return urlString
     }
 }
 
---
+++ FruitsHomeworkTests/FruitsHomeworkTests.swift
@@ -0,0 +1,121 @@
+//
+//  FruitsHomeworkTests.swift
+//  FruitsHomeworkTests
+//
+//  Created by Matvii Artemenko on 11/01/2024.
+//
+
+import Combine
+import XCTest
+@testable import FruitsHomework
+
+final class FruitsHomeworkTests: XCTestCase {
+    var cancellables: Set<AnyCancellable> = []
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+    func test_FruitsViewModel_getFruits_ShouldBeCorrectAmoutOfItemsFromAPI() {
+        // Given
+        let fruitVM = FruitsViewModel()
+        // When
+        fruitVM.getFruits()
+        // Then
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            XCTAssertEqual(fruitVM.fruits.count, 9)
+
+        }
+    }
+
+    func test_StatisticsManager_networkCallStats_TimesShouldBeLessThen() {
+        // Given
+        let manager = StatisticsManager()
+        let fruitVM = FruitsViewModel()
+        var expectation = XCTestExpectation(description: "Async operation expectation")
+
+        // When
+        let time = manager.networkCallStats {
+            fruitVM.getFruits()
+            expectation.fulfill()
+        }
+
+        // Then
+        wait(for: [expectation], timeout: 5)
+        XCTAssertLessThan(time, 1)
+    }
+
+    func test_StatisticsManager_displayEventStats_TimesShouldNotBeZero() {
+        // Given
+        let manager = StatisticsManager()
+        manager.startTime = .now()
+        var expectation = XCTestExpectation(description: "Async operation expectation")
+
+        // When
+        let duration = manager.displayEventStats()
+        expectation.fulfill()
+
+        // Then
+        wait(for: [expectation], timeout: 2)
+        XCTAssertNotEqual(duration, 0.0)
+    }
+
+    func test_StatisticsManager_getStats_urlShouldBeTheSame() {
+        // Given
+        let manager = StatisticsManager()
+        let randomData = Int.random(in: 1 ..< 9999)
+        let event = ScreeningEvents.display
+        let urlString = "https://raw.githubusercontent.com/fmtvp/recruit-test-data/master/stats?event=\(event.rawValue)&data=\(randomData)"
+
+        // When
+        let statUrl = manager.getStats(for: event, "\(randomData)")
+
+        // Then
+        XCTAssertEqual(statUrl, urlString)
+    }
+
+    func test_NetworkManager_getData_requestShouldNotFail() {
+        // Given
+        let expectation = XCTestExpectation(description: "Fetch data from the server")
+        let testURL = URL(string: "https://raw.githubusercontent.com/fmtvp/recruit-test-data/master/data.json")!
+
+        // When
+
+        Network.shared.getData(with: testURL)
+            .sink { completion in
+                switch completion {
+                case .failure(let error):
+                    XCTFail("Failed to fetch data with error: \(error)")
+                case .finished:
+                    print("Completion: \(completion)")
+
+                }
+                expectation.fulfill()
+            } receiveValue: { (_: FruitsResponse) in }
+            .store(in: &cancellables)
+
+        // Then
+
+        wait(for: [expectation], timeout: 5.0)
+
+    }
+}
